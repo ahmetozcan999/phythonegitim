e0a606e82d4dc01462c3a8037db80bc6f1cc039c
ahmetozcan999 <f111216079@ktun.edu.tr>
2021-05-23 15:41:39 +0300

    first commit

---
+++ degiskenler.py
@@ -0,0 +1,5 @@
+name = 'ahmet test'
+sayi = 12
+print(name)
+print(sayi)
+print(name + " " + sayi.__str__())
---
+++ sartbloklari.py
@@ -0,0 +1,9 @@
+# coding=UTF-8
+dolarDun = 7.35
+dolarBugun = 7.45
+
+if dolarDun > dolarBugun:
+    print("düştü")
+elif dolarDun < dolarBugun:
+    print ("yükseldi")
+else : print("sabit")
